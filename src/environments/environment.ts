efe94de50fef1948d4acf788707c8c20337c4eee
Warren <warren.sprau@gmail.com>
2020-10-02 15:36:44 -0700

    - set default env to equiv of devlocal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  API_BASE_URL: 'https://localhost:44340',  
-  AUTH0_DOMAIN: "whittle-dev-ws.auth0.com",
-  AUTH0_CLIENT_ID: "4oo4ElcjBRvCBuTDDkndXAyDniwwVUQT",
-  AUTH0_AUDIENCE: "https://api.whittle.life"
+  API_BASE_URL: 'https://localhost:44340',
+  AUTH0_DOMAIN: "whittle-life-dev.auth0.com",
+  AUTH0_CLIENT_ID: "CTadRzDqN7YeFQVztS3yOvfFpIgqiOk5",
+  AUTH0_AUDIENCE: "https://dev.local.api.whittle.life"
 };
 
 /*
